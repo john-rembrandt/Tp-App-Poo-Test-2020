a4a80ad31c2ec583c74ae66c7495018002615f51
john-rembrandt <sk8mystic@gmail.com>
2019-08-24 02:17:04 +0200

    modif sur PDO pour utilisé fetch_class

---
+++ NewsModel.php
@@ -0,0 +1,134 @@
+<?php
+
+class NewsModel
+{
+    /*
+    protected $erreurs = [],
+    $id,
+    $auteur,
+    $titre,
+    $contenu,
+    $dateAjout,
+    $dateModif;
+    */
+    
+    public $id;
+    public $auteur;
+    public $titre;
+    public $contenu;
+    public $dateAjout;
+    public $dateModif;
+    
+    public function __construct($id, $auteur, $titre, $contenu, $dateAjout, $dateModif)
+    {
+        
+        $this->setId($id);
+        $this->setAuteur($auteur);
+        $this->setTitre($titre);
+        $this->setContenu($contenu);
+        $this->setDateAjout($dateAjout);
+        $this->setDateModif($dateModif);
+        
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateAjout()
+    {
+        return $this->dateAjout;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateModif()
+    {
+        return $this->dateModif;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $auteur
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @param mixed $contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    }
+
+    /**
+     * @param mixed $dateAjout
+     */
+    public function setDateAjout($dateAjout)
+    {
+        $this->dateAjout = $dateAjout;
+    }
+
+    /**
+     * @param mixed $dateModif
+     */
+    public function setDateModif($dateModif)
+    {
+        $this->dateModif = $dateModif;
+    }
+
+    
+    
+    
+}
+
