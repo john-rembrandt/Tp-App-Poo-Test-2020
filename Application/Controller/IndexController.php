eac221989a8ec914fbe3cf5ed8b5fb7e379d7711
john-rembrandt <sk8mystic@gmail.com>
2020-02-01 17:27:55 +0100

    ajout de la function getController dans indexContller pour test

--- Application/Controller/IndexController.php
+++ Application/Controller/IndexController.php
@@ -34,14 +34,69 @@ class IndexController
         $this->page = new Page();
         $this->httpRequest = new HTTPRequest();
         $this->router = new Router();
-        $this->route = new Route();
+        
         
     }
-
+    
+    
+    public function getController()
+    {
+        $router = new Router;
+        
+        $xml = new \DOMDocument;
+        $xml->load(__DIR__.'/../../App/'.$this->name.'/Config/routes.xml');
+        
+        $routes = $xml->getElementsByTagName('route');
+        
+        // On parcourt les routes du fichier XML.
+        foreach ($routes as $route)
+        {
+            $vars = [];
+            
+            // On regarde si des variables sont présentes dans l'URL.
+            if ($route->hasAttribute('vars'))
+            {
+                $vars = explode(',', $route->getAttribute('vars'));
+            }
+            
+            // On ajoute la route au routeur.
+            $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+        }
+        
+        try
+        {
+            // On récupère la route correspondante à l'URL.
+            $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+        }
+        catch (\RuntimeException $e)
+        {
+            if ($e->getCode() == Router::NO_ROUTE)
+            {
+                // Si aucune route ne correspond, c'est que la page demandée n'existe pas.
+                $this->httpResponse->redirect404();
+            }
+        }
+        
+        // On ajoute les variables de l'URL au tableau $_GET.
+        $_GET = array_merge($_GET, $matchedRoute->vars());
+        
+        // On instancie le contrôleur.
+        $controllerClass = 'App\\'.$this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+    }
+    
+    
+    
     public function executeIndex()
     {
+        $url = $this->httpRequest->requestURI();
         
-        if($this->cacheIndex->dateCreationCache() == true)
+        //$this->route = new Route();
+        
+        var_dump($this->router->getRoute($url));
+        
+       
+        /* if($this->cacheIndex->dateCreationCache() == true)
         {
             $this->cacheIndex->lireCache();
         }
@@ -61,9 +116,9 @@ class IndexController
                 
                 $this->cacheIndex->creerCache($this->cacheIndex->fichierCache,
                 $this->page->affichageVue($this->donnee->getSelectionListe())); 
-            }
+            } 
              
-        }
+        }*/
       }
     
 }
