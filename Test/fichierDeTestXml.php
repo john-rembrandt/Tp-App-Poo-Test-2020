7e09b3eb9409a68f85f5b99b19715a0fc2b69cb9
john-rembrandt <sk8mystic@gmail.com>
2020-02-02 13:36:18 +0100

    pour test de lecture hors environnement

---
+++ Test/fichierDeTestXml.php
@@ -0,0 +1,26 @@
+<?php
+
+class TestXml
+{
+    public function getXml()
+    {
+        $xml = new DOMDocument;
+        $xml->load(__DIR__.'configNews2.xml');
+
+        $routes = $xml->getElementsByTagName('route');
+
+        foreach ($routes as $route)
+        {
+          //$vars = [];
+
+            var_dump($route);
+
+        }
+    
+    }
+    
+
+} 
+
+$testRouting = new TestXml();
+$testRouting->getXml(); 
