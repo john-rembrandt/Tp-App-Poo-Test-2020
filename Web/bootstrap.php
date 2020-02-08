ae40935890f721f4bb5a566b7b501f370a3ea355
john-rembrandt <sk8mystic@gmail.com>
2020-02-08 18:16:46 +0100

    ajout dans autoloader le dossier Test

--- Web/bootstrap.php
+++ Web/bootstrap.php
@@ -7,6 +7,9 @@ $OCFramLoader->register();
 $LibLoader = new SplClassLoader('Lib', '../');
 $LibLoader->register();
 
+$TestLoader = new SplClassLoader('Test', '../');
+$TestLoader->register();
+
 use Application\Controller\IndexController;
 
 $index = new IndexController();
